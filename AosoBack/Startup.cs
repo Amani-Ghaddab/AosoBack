d76ad913ffec1906f900d7177d5ab77062b468e0
Amani Ghaddab <ameni.ghaddabe@gmail.com>
2022-06-06 12:02:26 +0100

    first commit

--- AosoBack/Startup.cs
+++ AosoBack/Startup.cs
@@ -26,11 +26,12 @@ namespace DevOpsProject
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "DevOpsProject", Version = "v1" });
             });
-            services.AddControllers();
+
             services.AddCors(option =>
             {
                 option.AddDefaultPolicy(builder =>
